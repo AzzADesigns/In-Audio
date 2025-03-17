2cf92146a84b7b3ae586a9cc7d79ab4c939c9d87
M-Azariel <walter.azariel.moreno@gmail.com>
2025-03-17 01:24:44 -0300

    finalizando el banner para pantallas 2xl

--- AzzAudio/src/App.tsx
+++ AzzAudio/src/App.tsx
@@ -4,7 +4,7 @@ import { Banner } from "./components/Banner";
 function App() {
     return (
         <div className="min-h-screen flex-center bg-gradient-to-br from-[#00FFCC] via-[#02271F] via-[#00DEB2] via-[#020202] to-[#D9D9D9]">
-            <div className="bg-[#020202] h-[95%] w-[95%] flex-center">
+            <div className="bg-[#020202] h-[95%] w-[95%] flex-center mt-10 mb-10">
                 <header className="w-full flex-center">
                     <Banner/>
                 </header>
--- AzzAudio/src/components/Banner.tsx
+++ AzzAudio/src/components/Banner.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 
 export const Banner = () => {
     return (
-        <section className='flex-center w-full '>
-            <div className='2xl:w-full flex justify-between pl-[35%] items-start'>
-                <img src="/banner.png" alt="imagen del banner" className='2xl:w-[1000px] 2xl:pt-28' />
-                <button className='mt-10 mr-10'>
-                    <p className='text-tertiary text-6xl font-dots'>Play Music</p>
+        <section className='flex-center w-full mb-96 '>
+            <div className='2xl:w-full flex justify-between pl-[22%] items-start mr-12 '>
+                <img src="/banner.png" alt="imagen del banner" className='2xl:w-[1200px] ' />
+                <button className='mt-10 mr-5 cursor-pointer text-tertiary hover:text-secundary  '>
+                    <p className=' text-2xl font-dots underline underline-offset-10 decoration-4 transition-all duration-200 '>Play Music</p>
                 </button>
             </div>
-            <figure>
-                <img src="Logo1.svg" alt="" className='2xl:absolute 2xl:right-96 2xl:top-64 2xl:w-[45%] '/>
-            </figure>
+            <div>
+                <img src="Logo1.svg" alt="" className='2xl:absolute 2xl:right-96 2xl:top-52 2xl:w-[45%] '/>
+            </div>
+            <div className='absolute left-96 bottom-8'>
+                <p className='text-tertiary text-2xl font-uniq  tracking-[0.3em]'>“The sound of the unheard”</p>
+            </div>
         </section>
     )
 }
