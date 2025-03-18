ed4d39f9a3391c23b4cf2ddc75e71af0d453cfa6
M-Azariel <walter.azariel.moreno@gmail.com>
2025-03-18 00:18:35 -0300

    preparando el diseño movile

--- AzzAudio/src/components/Banner.tsx
+++ AzzAudio/src/components/Banner.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const Banner = () => {
     return (
         <section className='flex-center w-full mb-96 '>
-            <div className='2xl:w-full flex justify-between pl-[22%] items-start mr-12 '>
+            <div className='2xl:w-full flex justify-between pl-[22%] items-start 2xl:mr-12 pt-20 2xl:pt-0'>
                 <img src="/banner.png" alt="imagen del banner" className='2xl:w-[1200px] ' />
-                <button className='mt-10 mr-5 cursor-pointer text-tertiary hover:text-secundary  '>
-                    <p className=' text-2xl font-dots underline underline-offset-10 decoration-4 transition-all duration-200 '>Play Music</p>
+                <button className='2xl:mt-10 absolute top-10 2xl:top-0 2xl:relative mr-5 cursor-pointer text-tertiary hover:text-secundary  '>
+                    <p className=' 2xl:text-2xl font-dots underline underline-offset-10 decoration-4 transition-all duration-200 '>Play Music</p>
                 </button>
             </div>
             <div>
