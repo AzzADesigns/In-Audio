5cca491a37db08489f25c31ac03792af2ad69055
M-Azariel <walter.azariel.moreno@gmail.com>
2025-03-20 01:47:35 -0300

    navbar modificada con distinto contenido segun el tipo de tamaño de pantalal pc/movile

--- AzzAudio/src/components/NavBar.tsx
+++ AzzAudio/src/components/NavBar.tsx
@@ -1,26 +1,45 @@
 import { RiLoginCircleFill } from "react-icons/ri";
 import { BsFillPlayCircleFill } from "react-icons/bs";
 import { IoSettings } from "react-icons/io5";
+import { useEffect, useState } from "react";
+
+
+
+const liIcons=[
+    < RiLoginCircleFill className="w-6 h-6 text-tertiary hover:text-secundary hover:scale-105"/>,
+    <BsFillPlayCircleFill className="w-7 h-7 text-tertiary hover:text-secundary hover:scale-105"/>,
+    <IoSettings className="w-6 h-6 text-tertiary hover:text-secundary hover:scale-105"/>,
+]
+
+const liWords: string[] = [
+    "Login",
+    "Play Music",
+    "Settings",
+];
 
 export const NavBar = () => {
 
-    const liIcons=[
-        < RiLoginCircleFill/>,
-        <BsFillPlayCircleFill/>,
-        <IoSettings/>,
-    ]
+    const [navItems, setNavItems] = useState<(string | JSX.Element)[]>([]);
 
-    const liWords:[
-        "Login",
-        "Play Music",
-        "Settings",
-    ]
+    useEffect(() => {
+        const updateNavbar = () => {
+            setNavItems(window.innerWidth >= 1536 ? liWords : liIcons)
+        };
+    
+        updateNavbar();
+        window.addEventListener("resize",updateNavbar);
+        return ()=> window.removeEventListener("resize", updateNavbar);
+    },[])
 
 
     return (
-        <div className='text-tertiary fixed 2xl:bottom-20 flex-center bg-black/50 backdrop-blur-xl h-20 w-[40%] px-16 rounded-full border-4 border-secundary'>
-            <ul className='flex 2xl:w-full justify-between 2xl:text-3xl [&_li]:cursor-pointer [&_li]:flex [&_li]:justify-center [&_li]:items-center [&_li]:hover:text-secundary [&_li]:hover:scale-105 [&_li]:transition-all [&_li]:duration-300'>
-                
+        <div className='text-tertiary fixed 2xl:bottom-20 flex justify-center items-center bg-black/50 backdrop-blur-xl h-12 2xl:h-20  w-[200px] 2xl:w-[40%] px-16 rounded-full border-4 border-secundary'>
+            <ul className='flex w-[200px] 2xl:w-full justify-between items-center 2xl:text-3xl   '>
+                {navItems.map((item, index)=>(
+                        <li className="flex w-[150px] 2xl:w-auto justify-center  2xl:gap-52 items-center cursor-pointer hover:text-secundary hover:scale-105 transition-all duration-300 font-uniq" key={index}>
+                            {item}
+                        </li>
+                ))}
             </ul>
         </div>
     )
