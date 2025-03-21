27c0ae715fede84d7a89b7e4cc285502c20ab47f
M-Azariel <walter.azariel.moreno@gmail.com>
2025-03-21 13:17:59 -0300

    terminados todos los breackpoints responsive del headder y la navbar

--- AzzAudio/src/App.tsx
+++ AzzAudio/src/App.tsx
@@ -4,7 +4,7 @@ import { NavBar } from "./components/NavBar";
 
 function App() {
     return (
-        <div className="min-h-screen flex-center bg-gradient-to-br from-[#00FFCC] via-[#02271F] via-[#00DEB2] via-[#020202] to-[#D9D9D9]">
+        <div className="min-h-screen flex-center bg-[linear-gradient(to_bottom_right,#013027,#01846a,#00c8a0,#017c63)]">
             <div className="bg-[#020202] h-[95%] w-[95%] flex-center mt-2 2xl:mt-10 2xl:mb-10">
                 <NavBar/>
                 <header className="w-full flex-center">
