82f98925eca0c6c5c991fa2d3d5e9280cc6284ca
M-Azariel <walter.azariel.moreno@gmail.com>
2025-03-22 02:43:08 -0300

    agregando efecto en bg y solucionando problemas de posicionamiento de divs que surgieron por poner relative en el padre

--- AzzAudio/src/components/header/Banner.tsx
+++ AzzAudio/src/components/header/Banner.tsx
@@ -13,17 +13,18 @@ export const Banner = () => {
 
     return (
         <section className='flex-center w-full mb-96 '>
-            <div className='2xl:w-full flex justify-between 2xl:pl-[19%] items-start 2xl:mr-12  2xl:pt-0'>
-                <img src="/banner.png" alt="imagen del banner" className='2xl:w-[1200px] ' />
+            <div className='2xl:w-full flex justify-between 2xl:pl-[19%] items-start 2xl:mr-12  2xl:pt-5'>
+                {/*falta centrar el banner */}
+                <img src="/banner.png" alt="imagen del banner" className='2xl:w-[1300px] ' />
                 <button className='2xl:mt-10 absolute top-10 2xl:top-0 2xl:relative mr-5 cursor-pointer text-tertiary hover:text-secundary  '>
                     <p className=' xl:w-44 2xl:text-2xl font-dots underline underline-offset-10 decoration-4 transition-all duration-200 hidden 2xl:flex'>See genres</p>
                 </button>
             </div>
             <div className='absolute top-20 2xl:static 2xl:h-auto '>
-                <img src="Logo1.svg" alt="" className='md:ml-[24%] 2xl:ml-0  2xl:absolute  2xl:left-[32.4%] 2xl:top-52 w-52 md:w-[50%] xl:w-[50%] 2xl:w-[35%]'/>
+                <img src="Logo1.svg" alt="" className='md:ml-[24%] 2xl:ml-0  2xl:absolute  2xl:left-[35%] 2xl:top-32 w-52 md:w-[50%] xl:w-[50%] 2xl:w-[30%]'/>
             </div>
-            <div className='absolute 2xl:static 2xl:left-0 left-[5%] bottom-0'>
-                <p className='text-tertiary absolute   bottom-96 2xl:bottom-5 2xl:left-16 text-xs md:text-lg 2xl:text-xl font-uniq  tracking-[0.3em]'>“The sound of the unheard”</p>
+            <div className='absolute lg:w-10 xl:w-96 2xl:w-[500px] lg:top-[65%]  2xl:top-[50%] 2xl:left-0 left-[5%] bottom-0'>
+                <p className='hidden lg:flex text-tertiary z-10 relative    bottom-96 2xl:bottom-5 2xl:left-16 text-xs md:text-lg 2xl:text-xl font-uniq  tracking-[0.3em]'>“The sound of the unheard”</p>
             </div>
             <figure className=" flex justify-center">
                 <svg
