30dbf608d178ccd66e36a213926bd704afa9bc7f
M-Azariel <walter.azariel.moreno@gmail.com>
2025-03-28 18:52:37 -0300

    arreglando el responsive

--- AzzAudio/src/components/main/layaout-main/sectionText/Text.tsx
+++ AzzAudio/src/components/main/layaout-main/sectionText/Text.tsx
@@ -1,7 +1,7 @@
 
 export const Text = () => {
     return (
-        <div className="w-full 2xl flex-center bg-primary  text-tertiary 2xl:text-5xl font-dots ">
+        <div className="w-full 2xl h-96  flex-center bg-primary  text-tertiary md:text-2xl lg:text-4xl xl:text-4xl 2xl:text-5xl font-dots ">
             <p className="leading-tight tracking-wider">
                 "Unleashing the <span className="text-secundary">sound </span>  of tomorrow - <br/> 
                 <span className="text-secundary">empowering</span> artists, <span className="text-secundary">conecting</span> global audiences, <br/>
