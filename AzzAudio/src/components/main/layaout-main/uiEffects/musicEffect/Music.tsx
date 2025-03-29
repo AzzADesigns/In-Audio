3a9b4b9db380fd772395bdce42235dc833c0097f
M-Azariel <walter.azariel.moreno@gmail.com>
2025-03-29 19:19:36 -0300

    comienzo de efecto de musica en el texto

---
+++ AzzAudio/src/components/main/layaout-main/uiEffects/musicEffect/Music.tsx
@@ -0,0 +1,10 @@
+import { CubeGrid } from "./CubeGrid"
+
+export const Music = () => {
+    return (
+        <div className="inline-flex  gap-2 items-end">
+            <CubeGrid />
+
+        </div>
+    );
+};
