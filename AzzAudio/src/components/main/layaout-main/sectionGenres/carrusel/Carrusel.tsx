6167f96f5894039a5bc075de61e5ce7c67535666
M-Azariel <walter.azariel.moreno@gmail.com>
2025-04-08 01:15:21 -0300

    agregando la data y probando que funcione

--- AzzAudio/src/components/main/layaout-main/sectionGenres/carrusel/Carrusel.tsx
+++ AzzAudio/src/components/main/layaout-main/sectionGenres/carrusel/Carrusel.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import {genres} from "./data/gneres"
 
 export const Carrusel = () => {
-  return (
-    <div className='flex-center h-[600px] w-full bg-red-900'>
-
-    </div>
-  )
+    return (
+        <div className='flex-center h-[600px] w-full bg-red-900'>
+            <ul className="flex">
+                {genres.map((genre)=>(
+                    <li key={genre.genre}>{genre.genre}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
