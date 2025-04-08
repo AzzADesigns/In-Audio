a38a1d096f71bda38323a7c6eb352f671c42cc74
M-Azariel <walter.azariel.moreno@gmail.com>
2025-04-07 23:36:40 -0300

    comenzando carrusel

--- AzzAudio/src/components/main/layaout-main/sectionGenres/TitleAndPresentation/TitleSectionGenre.tsx
+++ AzzAudio/src/components/main/layaout-main/sectionGenres/TitleAndPresentation/TitleSectionGenre.tsx
@@ -4,10 +4,10 @@ export const TitleSectionGenre = () => {
     return (
         <div className="font-dots 2xl:w-3xl text-tertiary md:ml-auto text-2xl md:text-7xl 2xl:text-8xl [&_span]:text-secundary ">
             <h1 className="w-full  inline-flex items-baseline">
-                <span className="mr-1  2xl:ml-auto 2xl:mr-4"><LogoFace/></span> Ur
+                <span className="mr-1 "><LogoFace/></span> Ur
             </h1>
-            <h1 className=" 2xl:ml-64">
-                <span className="ml-16 md:ml-28">G</span>enres
+            <h1 className=" ">
+                <span className="ml-16 md:ml-28 2xl:ml-64">G</span>enres
             </h1>
         </div>
     )
---
+++ AzzAudio/src/components/main/layaout-main/sectionGenres/carrusel/Carrusel.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Carrusel = () => {
+  return (
+    <div className='flex-center h-[600px] w-full bg-red-900'>
+
+    </div>
+  )
+}
