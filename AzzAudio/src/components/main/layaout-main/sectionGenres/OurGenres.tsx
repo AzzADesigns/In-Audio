3129a7115f7269a3ff0a86b4c87077e34560b33b
M-Azariel <walter.azariel.moreno@gmail.com>
2025-04-14 20:26:11 -0300

    se pulio la maquetacion y se dejo practicamente perfecta, faltan dedetalles

--- AzzAudio/src/components/main/layaout-main/sectionGenres/OurGenres.tsx
+++ AzzAudio/src/components/main/layaout-main/sectionGenres/OurGenres.tsx
@@ -5,7 +5,7 @@ import { TitleSectionGenre } from "./TitleAndPresentation/TitleSectionGenre"
 export const OurGenres = () => {
     return (
         <div className=" w-full">
-            <section className="flex flex-col md:flex-row md:justify-center md:items-center gap-10 md:gap-16 lg:gap-20 xl:justify-between">
+            <section className="flex flex-col md:flex-row md:justify-between md:items-center gap-10 md:gap-16 lg:gap-20 xl:justify-between">
                 <div className="">
                     <TitleSectionGenre/>
                 </div>
